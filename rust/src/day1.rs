e86e439f57f2c58d3359b9c0e8dfd295eab5e2b7
Colin Harrington <colin.harrington@gmail.com>
2023-09-23 02:07:38 -0500

    Day1 tests

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -27,3 +27,36 @@ pub fn solve_part2(input: &str) -> u32 {
             }
         }).unwrap() as u32
 }
+
+#[cfg(test)]
+mod test {
+    use crate::day1::{solve_part1, solve_part2};
+
+    /// For example:
+    ///  - `(())` and `()()` both result in floor `0`.
+    ///  - `(((` and `(()(()(` both result in floor `3`.
+    ///  - `))(((((` also results in floor `3`.
+    ///  - `())` and `))(` both result in floor `-1` (the first basement level).
+    ///  - `)))` and `)())())` both result in floor `-3`.
+    #[test]
+    fn part1_examples() {
+        assert_eq!(0, solve_part1("(())"));
+        assert_eq!(0, solve_part1("()()"));
+        assert_eq!(3, solve_part1("((("));
+        assert_eq!(3, solve_part1("(()(()("));
+        assert_eq!(3, solve_part1("(()(()("));
+        assert_eq!(-1, solve_part1("())"));
+        assert_eq!(-1, solve_part1("))("));
+        assert_eq!(-3, solve_part1(")))"));
+        assert_eq!(-3, solve_part1(")())())"));
+    }
+
+    /// For example:
+    /// - `)` causes him to enter the basement at character position `1`.
+    /// - `()())` causes him to enter the basement at character position `5`.
+    #[test]
+    fn part2_examples() {
+        assert_eq!(1, solve_part2(")"));
+        assert_eq!(5, solve_part2("()())"));
+    }
+}
