69901f8abc13e29c302f6d4f8e1c5cf2422c5bd0
Colin Harrington <colin.harrington@gmail.com>
2023-09-24 15:24:56 -0500

    Day 5 part2 solution, cleanup, format, etc.

--- rust/src/day5.rs
+++ rust/src/day5.rs
@@ -1,5 +1,6 @@
 use aoc_runner_derive::aoc;
 use itertools::Itertools;
+use std::collections::HashMap;
 
 #[aoc(day5, part1)]
 pub fn solve_part1(input: &str) -> usize {
@@ -13,31 +14,57 @@ pub fn solve_part1(input: &str) -> usize {
 
 fn at_least_3_vowels(text: &&str) -> bool {
     text.chars()
-        .filter(|c| match c {
-            'a' | 'e' | 'i' | 'o' | 'u' => true,
-            _ => false,
-        })
+        .filter(|c| matches!(c, 'a' | 'e' | 'i' | 'o' | 'u'))
         .count()
         >= 3
 }
 
 fn duplicate_letters(text: &&str) -> bool {
-    text.chars().tuple_windows().find(|(a, b)| a == b).is_some()
+    text.chars().tuple_windows().any(|(a, b)| a == b)
 }
 
 fn not_restricted(text: &&str) -> bool {
     text.chars()
         .tuple_windows()
-        .find(|&pair| match pair {
-            ('a', 'b') | ('c', 'd') | ('p', 'q') | ('x', 'y') => true,
-            _ => false,
-        })
-        .is_none()
+        .all(|pair: CharPair| !matches!(pair, ('a', 'b') | ('c', 'd') | ('p', 'q') | ('x', 'y')))
+}
+
+#[aoc(day5, part2)]
+pub fn solve_part2(input: &str) -> usize {
+    input
+        .lines()
+        .filter(paired_twice)
+        .filter(paired_triplet)
+        .count()
+}
+
+type CharPair = (char, char);
+
+fn paired_twice(text: &&str) -> bool {
+    let mut pairs: HashMap<CharPair, usize> = HashMap::new();
+
+    text.chars().tuple_windows().enumerate().any(|(idx, pair)| {
+        if let Some(existing_idx) = pairs.get(&pair) {
+            idx - existing_idx >= 2
+        } else {
+            pairs.insert(pair, idx);
+            false
+        }
+    })
+}
+fn paired_triplet(text: &&str) -> bool {
+    text.chars()
+        .collect_vec()
+        .windows(3)
+        .any(|w| w.len() == 3 && w[0] == w[2])
 }
 
 #[cfg(test)]
 mod test {
-    use crate::day5::{at_least_3_vowels, duplicate_letters, not_restricted, solve_part1};
+    use crate::day5::{
+        at_least_3_vowels, duplicate_letters, not_restricted, paired_triplet, paired_twice,
+        solve_part1, solve_part2,
+    };
 
     #[test]
     fn test_vowels() {
@@ -70,5 +97,31 @@ mod test {
         .join("\n");
 
         assert_eq!(2, solve_part1(input.as_str()));
+    }
+
+    #[test]
+    fn duplicate_twice() {
+        assert_eq!(true, paired_twice(&"xyxy"));
+        assert_eq!(true, paired_twice(&"aabcdefgaa"));
+        assert_eq!(false, paired_twice(&"aaa"));
+    }
+
+    #[test]
+    fn separated_pair() {
+        assert_eq!(true, paired_triplet(&"xyx"));
+        assert_eq!(true, paired_triplet(&"abcdefeghi"));
+        assert_eq!(true, paired_triplet(&"aaa"));
+    }
+    #[test]
+    fn part2() {
+        let input = [
+            "qjhvhtzxzqqjkmpb",
+            "xxyxx",
+            "uurcxstgmygtbstg",
+            "ieodomkazucvgmuy",
+        ]
+        .join("\n");
+
+        assert_eq!(2, solve_part2(input.as_str()));
     }
 }
