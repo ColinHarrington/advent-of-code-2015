9823f80cdeda3438f94fdc01db4a8c653aab030b
Colin Harrington <colin.harrington@gmail.com>
2023-10-10 02:19:37 -0500

    Day19 Part2 | Medicine for Rudolph - Bundled input into struct - Used random shuffle to work through reverse replacements.
    ```
    AOC 2015
    Day 19 - Part 1 : 576
            generator: 12.219µs,
            runner: 563.997µs
    
    Day 19 - Part 2 : 207
            generator: 9.5µs,
            runner: 122.041µs
    ```

--- rust/src/day19.rs
+++ rust/src/day19.rs
@@ -1,4 +1,4 @@
-use aoc_runner_derive::aoc;
+use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 use nom::bytes::complete::tag;
 use nom::character::complete::{alpha1, line_ending};
@@ -6,52 +6,98 @@ use nom::combinator::map;
 use nom::multi::{many1, separated_list1};
 use nom::sequence::separated_pair;
 use nom::IResult;
+use rand::seq::SliceRandom;
+
+#[aoc_generator(day19)]
+fn parse_rudolph_meds(input: &str) -> RudolphMeds {
+    rudolph_meds(input).unwrap().1
+}
 
 #[aoc(day19, part1)]
-fn solve_part1(input: &str) -> usize {
-    let (replacements, subject) = parse_input(input.trim()).unwrap().1;
-    replacements
+fn solve_part1(input: &RudolphMeds) -> usize {
+    input
+        .replacements
         .iter()
-        .flat_map(|repl| repl.matches(subject))
+        .flat_map(|repl| repl.matches(input.medicine.as_str()))
         .unique()
         .count()
 }
 
 #[aoc(day19, part2)]
-fn solve_part2(input: &str) -> usize {
-    let (replacements, destination) = parse_input(input.trim()).unwrap().1;
-    replacements
-        .iter()
-        .flat_map(|repl| repl.matches(destination))
-        .unique()
-        .count()
+pub fn part2(input: &RudolphMeds) -> usize {
+    let mut replacements = input.replacements.clone();
+
+    let mut rng = rand::thread_rng();
+
+    let mut molecule = String::new();
+    loop {
+        let mut steps = 0;
+        let mut mol = input.medicine.clone();
+        replacements.shuffle(&mut rng);
+        loop {
+            for r in &replacements {
+                let from = r.to.as_str();
+                let to = r.from.as_str();
+                let matches = mol.clone().matches(from).count();
+                let next = mol.replace(from, to);
+                if next == "e" {
+                    return steps + matches;
+                } else {
+                    if next.contains('e') {
+                        continue;
+                    }
+                    mol = next;
+                    steps += matches
+                }
+            }
+            if molecule == mol {
+                break;
+            }
+            molecule = mol.clone();
+        }
+    }
 }
 
-fn parse_input(input: &str) -> IResult<&str, (Vec<Replacement>, &str)> {
-    separated_pair(replacements, many1(line_ending), alpha1)(input)
-}
-fn replacements(input: &str) -> IResult<&str, Vec<Replacement>> {
-    separated_list1(line_ending, replacement)(input)
-}
-fn replacement(input: &str) -> IResult<&str, Replacement> {
+fn rudolph_meds(input: &str) -> IResult<&str, RudolphMeds> {
     map(
-        separated_pair(alpha1, tag(" => "), alpha1),
-        |(token, replacement)| Replacement {
-            from: token,
-            to: replacement,
+        separated_pair(replacements, many1(line_ending), alpha1),
+        |(replacements, med)| RudolphMeds {
+            replacements,
+            medicine: med.to_string(),
         },
     )(input)
 }
-#[derive(Debug)]
-struct Replacement<'a> {
-    from: &'a str,
-    to: &'a str,
+
+fn replacements(input: &str) -> IResult<&str, Vec<Replacement>> {
+    separated_list1(line_ending, replacement)(input)
 }
 
-impl Replacement<'_> {
+fn replacement(input: &str) -> IResult<&str, Replacement> {
+    map(
+        separated_pair(alpha1, tag(" => "), alpha1),
+        |(token, replacement): (&str, &str)| Replacement {
+            from: token.to_string(),
+            to: replacement.to_string(),
+        },
+    )(input)
+}
+
+#[derive(Debug)]
+pub struct RudolphMeds {
+    replacements: Vec<Replacement>,
+    medicine: String,
+}
+
+#[derive(Debug, Clone)]
+struct Replacement {
+    from: String,
+    to: String,
+}
+
+impl Replacement {
     fn matches(&self, subject: &str) -> Vec<String> {
         subject
-            .match_indices(self.from)
+            .match_indices(self.from.as_str())
             .map(|(match_idx, s)| {
                 format!(
                     "{}{}{}",
@@ -66,7 +112,7 @@ impl Replacement<'_> {
 
 #[cfg(test)]
 mod test {
-    use crate::day19::{parse_input, solve_part1};
+    use crate::day19::{parse_rudolph_meds, rudolph_meds, solve_part1};
 
     const EXAMPLE: &str = r"H => HO
 H => OH
@@ -75,14 +121,15 @@ O => HH
 HOH";
     #[test]
     fn parse_example() {
-        let (tail, (replacements, subject)) = parse_input(EXAMPLE.trim()).unwrap();
+        let (tail, red_meds) = rudolph_meds(EXAMPLE.trim()).unwrap();
         assert_eq!("", tail);
-        assert_eq!("HOH", subject);
-        assert_eq!(3, replacements.len());
+        assert_eq!("HOH", red_meds.medicine.as_str());
+        assert_eq!(3, red_meds.replacements.len());
     }
 
     #[test]
     fn example() {
-        assert_eq!(4, solve_part1(EXAMPLE));
+        let rudolph_meds = parse_rudolph_meds(EXAMPLE.trim());
+        assert_eq!(4, solve_part1(&rudolph_meds));
     }
 }
